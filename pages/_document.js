5a4eb78d587d559194d3a8e44c96352e33c0c03f
bestfindsuk <bestfindsuk@proton.me>
2025-12-17 20:14:07 +0000

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,10 +2,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
-      <Head>
-        <meta name="robots" content="noindex, nofollow" />
-      </Head>
+    <Html lang="en-GB">
+      <Head />
       <body>
         <Main />
         <NextScript />
