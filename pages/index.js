cd5af452d94139ded28f544c056e3318e0ef415b
bestfindsuk <bestfindsuk@proton.me>
2025-12-17 16:28:04 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
-export default function Home() {
-  return (
-    <main style={{ maxWidth: 800, margin: "0 auto", padding: "2rem" }}>
-      <h1>BestFinds UK</h1>
-      <p>Working.</p>
-    </main>
-  );
-}
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/beauty/concealers/clean-under-60");
+  }, [router]);
+
+  return null;
+}
