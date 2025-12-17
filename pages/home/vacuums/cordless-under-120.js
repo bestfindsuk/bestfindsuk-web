57658e9ae18d2bc6013b50e4384f4b942f32413f
bestfindsuk <bestfindsuk@proton.me>
2025-12-17 21:08:05 +0000

    Update cordless-under-120.js

--- pages/home/vacuums/cordless-under-120.js
+++ pages/home/vacuums/cordless-under-120.js
@@ -101,6 +101,11 @@ export default function CordlessUnder120({ products, lastUpdated }) {
       <p>
         <strong>Amazon disclosure:</strong> As an Amazon Associate, BestFinds UK earns from qualifying purchases.
       </p>
+<p>
+  Also see: <a href="/beauty/concealers/clean-under-60">Clean beauty concealers under £60</a>
+</p>
+
+          
     </main>
   );
 }
